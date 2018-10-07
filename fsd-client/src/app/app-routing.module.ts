2fee3dea21bafb3d2e6bf359f47d231b961bd472
thallapalli <thallapallikarnakar@gmail.com>
2018-10-07 16:10:34 -0500

    car-edit removed cleanup

--- fsd-client/src/app/app-routing.module.ts
+++ fsd-client/src/app/app-routing.module.ts
@@ -7,11 +7,9 @@ import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 import {UrlPermission} from "./urlPermission/url-permission";
 import {CarListComponent} from "./components/car-list/car-list.component";
-import { CarEditComponent } from './components/car-edit/car-edit.component';
 import { PoliciesComponent } from './components/policies/policies.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { HomeComponent } from './components/home/home.component';
-import {CustomInterceptorService} from "./services/custom-interceptor.service";
 
 
 
@@ -21,14 +19,7 @@ const routes: Routes = [
   { path: 'adminview', component: AdminviewComponent ,canActivate: [UrlPermission] },
   { path: 'cars', component: CarListComponent,canActivate: [UrlPermission]},
   { path: 'policies', component: PoliciesComponent,canActivate: [UrlPermission]},
-  {
-    path: 'car-add',
-    component: CarEditComponent,canActivate: [UrlPermission]
-  },
-  {
-    path: 'car-edit/:id',
-    component: CarEditComponent,canActivate: [UrlPermission]
-  },
+ 
   {
 path: '',
 component: HomeComponent,canActivate: [UrlPermission]
--- fsd-client/src/app/app.module.ts
+++ fsd-client/src/app/app.module.ts
@@ -11,38 +11,21 @@ import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 
 import { AuthService } from "./services/auth.service";
-import {HttpModule} from "@angular/http";
+import { HttpModule } from '@angular/http';
 import {AccountService} from "./services/account.service";
-import {CustomInterceptorService} from "./services/custom-interceptor.service";
-import {
-  HttpRequest,
-  HttpHandler,
-  HttpEvent,
-  HttpInterceptor
-} from '@angular/common/http';
-
 import { AppRoutingModule } from './/app-routing.module';
 import { FormsModule } from '@angular/forms';
 import {FacebookModule} from "ngx-facebook";
 import {UrlPermission} from "./urlPermission/url-permission";
-
-import {
-  HttpRequest,
-  HttpHandler,
-  HttpEvent,
-  HttpClientModule,
-  HttpInterceptor
-} from '@angular/common/http';
 import { CarService } from './services/car.service';
 import { PolicyService } from './services/policy.service';
 import { CarListComponent } from './components/car-list/car-list.component';
 import { MatButtonModule, MatCardModule, MatInputModule, MatListModule, MatToolbarModule } from '@angular/material';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { CarEditComponent } from './components/car-edit/car-edit.component';
+
 import { PoliciesComponent } from './components/policies/policies.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { HomeComponent } from './components/home/home.component';
-import { HTTP_INTERCEPTORS } from '@angular/common/http';
 
 
 
@@ -54,7 +37,8 @@ import { HTTP_INTERCEPTORS } from '@angular/common/http';
     LoginComponent,
     RegisterComponent,
     CarListComponent,
-    CarEditComponent,
+    
+   
     PoliciesComponent,
     NavbarComponent,
     HomeComponent,
@@ -64,10 +48,10 @@ import { HTTP_INTERCEPTORS } from '@angular/common/http';
   ],
   imports: [
     BrowserModule,
-    HttpModule,
+    
     FormsModule,
     AppRoutingModule,
-    HttpClientModule,
+    HttpModule,
     BrowserAnimationsModule,
     MatButtonModule,
     MatCardModule,
@@ -78,12 +62,7 @@ import { HTTP_INTERCEPTORS } from '@angular/common/http';
   
     NgbModule.forRoot(),
   ],
-  providers: [AuthService,AccountService,UrlPermission,CarService, 
-  {
-      provide: HTTP_INTERCEPTORS,
-      useClass: CustomInterceptorService,
-      multi: true
-    }
+  providers: [AuthService,AccountService,UrlPermission,CarService
        
     ],
   bootstrap: [AppComponent]
