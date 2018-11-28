d756ef7deeda49319f56e6ce524393ef3899cbb0
thallapalli <thallapallikarnakar@gmail.com>
2018-11-27 20:55:02 -0600

    JUnit test cases added for policy service

--- fsd-service/src/test/java/com/cts/osp/kt/osp/FsdServiceApplicationTests.java
+++ fsd-service/src/test/java/com/cts/osp/kt/osp/FsdServiceApplicationTests.java
@@ -1,9 +1,14 @@
 package com.cts.osp.kt.osp;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import com.cts.osp.kt.osp.entity.Policy;
+import com.cts.osp.kt.osp.service.PolicyService;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -11,6 +16,42 @@ public class FsdServiceApplicationTests {
 
 	@Test
 	public void contextLoads() {
+	}
+
+	@Autowired
+	PolicyService policyService;
+	//Success scenario
+	@Test
+	public void testfindById() {
+		Policy policy = policyService.findById(400L);
+		Assert.assertEquals("wqeqwe2", policy.getDetails());
+	}
+	
+	@Test
+	public void testErrorfindById() {
+		Policy policy = policyService.findById(400L);
+		Assert.assertNotEquals("Negetive scenario", policy.getDetails());
+	}
+	@Test
+	public void testupdate() {
+		Policy policy=new Policy();
+		policy.setId(401L);
+		policy.setName("name401");
+		
+		Policy policy2 = policyService.update(policy);
+		Assert.assertEquals("name401", policy2.getName());
+	}
+	
+	@Test
+	public void testErrorupdate() {
+		Policy policy=new Policy();
+		policy.setId(40155L);
+		policy.setName("name401");
+		
+		Policy policy2 = policyService.update(policy);
+		policy2.getName();
+		
+		Assert.assertNotEquals("name401", policy2.getName());
 	}
 
 }
