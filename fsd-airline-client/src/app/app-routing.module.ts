36eb3d2089eb236ec51fc3d1ba197656b165fe13
thallapalli <thallapallikarnakar@gmail.com>
2018-12-09 15:05:16 -0600

    navigation added

--- fsd-airline-client/src/app/app-routing.module.ts
+++ fsd-airline-client/src/app/app-routing.module.ts
@@ -9,6 +9,8 @@ import {UrlPermission} from "./urlPermission/url-permission";
 import { PoliciesComponent } from './components/policies/policies.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { HomeComponent } from './components/home/home.component';
+import { BookingComponent } from './components/booking/booking.component';
+import { FlightComponent } from './components/flight/flight.component'
 
 
 
@@ -17,6 +19,8 @@ const routes: Routes = [
   { path: 'userview', component: UserviewComponent, canActivate: [UrlPermission] },
   { path: 'adminview', component: AdminviewComponent ,canActivate: [UrlPermission] },
    { path: 'policies', component: PoliciesComponent,canActivate: [UrlPermission]},
+    { path: 'mybookings', component: BookingComponent,canActivate: [UrlPermission]},
+     { path: 'flights', component: FlightComponent,canActivate: [UrlPermission]},
  
   {
 path: '',
--- fsd-airline-client/src/app/model/booking.ts
+++ fsd-airline-client/src/app/model/booking.ts
@@ -1,2 +1,15 @@
 export class Booking {
+		departureDate:string="";
+		numOfPassengers:string="";
+		totalCost:string="";
+		airline:string="";
+		arrivalTime:string="";
+		deaprtureTime:string="";
+		source:string="";
+		destination:string="";
+		price:string="";
+		Date = new Date();
+		bookingId:number;
+		
+
 }
