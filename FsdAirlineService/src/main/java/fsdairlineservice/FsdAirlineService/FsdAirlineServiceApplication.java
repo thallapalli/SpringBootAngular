dbe43992cb0643fe6bbc51852b9c2310b1860e2a
thallapalli <thallapallikarnakar@gmail.com>
2018-12-08 20:59:35 -0600

    FsdAirLineService Added -Initial checkin

---
+++ FsdAirlineService/src/main/java/fsdairlineservice/FsdAirlineService/FsdAirlineServiceApplication.java
@@ -0,0 +1,12 @@
+package fsdairlineservice.FsdAirlineService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FsdAirlineServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FsdAirlineServiceApplication.class, args);
+	}
+}
